616d1da43f838cec84b6d3ccc8df9ff0e0c9b62a
Rafael <rafael.oelmann@gmail.com>
2020-11-11 15:58:14 +0000

    added player1 and player2 form, tests pass

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'enter their names' do
+  scenario 'entering name1 and name2' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
